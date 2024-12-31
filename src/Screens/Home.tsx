957ec818fa99c338eb7b3f31678153faf3e63c50
Marvindlls <marvindillenseger@gmail.com>
2024-12-31 14:16:15 +0100

    Enhance Home screen: add logo, improve button styles, and integrate custom font

--- src/Screens/Home.tsx
+++ src/Screens/Home.tsx
@@ -1,9 +1,21 @@
-import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import { View, Text, StyleSheet, TouchableOpacity, Image } from "react-native";
 import { LinearGradient } from 'expo-linear-gradient';
 import Colors from "../Components/Colors";
+import { Roboto_700Bold, useFonts } from '@expo-google-fonts/roboto';
+
+const registerLogo = require('../../assets/registerLogo.png');
 
 
 function Home() {
+
+    let [fontsLoaded] = useFonts({
+        Roboto_700Bold,
+      });
+    
+      if (!fontsLoaded) {
+        return null;
+      }
+
     return(
         <LinearGradient
             colors={[Colors.Gradient1, Colors.Gradient2]}
@@ -13,11 +25,12 @@ function Home() {
 
         <View style={styles.button}>
             <TouchableOpacity style={styles.buttonOne}>
-                <Text>Se Connecter</Text>
+                <Text style={styles.textInput}>Se Connecter</Text>
             </TouchableOpacity>
 
             <TouchableOpacity style={styles.buttonTwo}>
-                <Text>S'inscrire</Text>
+                <Text style={styles.textInput}>S'inscrire</Text>
+                <Image source={registerLogo} style={{width: 24, height: 24}} />
             </TouchableOpacity>
         </View>
 
@@ -36,25 +49,39 @@ const styles = StyleSheet.create({
     },
     button: {
         flexDirection: 'row',
-        bottom: 50,
+        bottom: 55,
+        width: '80%',
+        justifyContent: 'space-between',
     },
     buttonOne: {
         height: 50,
         borderColor: '#d9d9D9',
         borderWidth: 1,
-        borderRadius: 5,
+        borderRadius: 25,
+        padding: 15,
+        width: '45%',
         marginBottom: 15,
-        paddingLeft: 10,
-        backgroundColor: 'white'
+        backgroundColor: 'white',
+        alignItems: 'center',
+        justifyContent: 'center'
     },
     buttonTwo: {
         height: 50,
         borderColor: '#d9d9D9',
         borderWidth: 1,
-        borderRadius: 5,
+        borderRadius: 25,
+        width: '45%',
+        padding: 15,
         marginBottom: 15,
-        paddingLeft: 10,
-        backgroundColor: 'white'
+        backgroundColor: 'white',
+        alignItems: 'center',
+        justifyContent: 'center',
+        gap: 3,
+        flexDirection: 'row-reverse'
+    },
+    textInput: {
+        fontSize: 15,
+        fontFamily: 'Roboto_700Bold',
     }
 })
 
