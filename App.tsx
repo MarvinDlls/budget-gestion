4b80bbd5e23507e477b8b09c2682145ef87cd406
Marvindlls <marvindillenseger@gmail.com>
2025-02-23 20:28:05 +0100

    Refactor App component to use StackNavigator for improved navigation structure

--- App.tsx
+++ App.tsx
@@ -2,13 +2,8 @@ import React from 'react';
 import { StyleSheet } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
 import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import Register from './src/Screens/Register';
-import Home from './src/Screens/Home';
-import Welcome from './src/Screens/Welcome';
+import StackNavigator from './src/navigation/StackNavigator';
 import Colors from './src/Components/Colors';
-
-const Stack = createStackNavigator();
 
 export default function App() {
   return (
@@ -17,16 +12,7 @@ export default function App() {
       style={styles.container}
     >
       <NavigationContainer>
-        <Stack.Navigator
-          initialRouteName='Welcome'
-          screenOptions={{
-            headerShown: false,
-          }}
-        >
-          <Stack.Screen name="Home" component={Home} />
-          <Stack.Screen name="Register" component={Register} />
-          <Stack.Screen name='Welcome' component={Welcome} />
-        </Stack.Navigator>
+        <StackNavigator />
       </NavigationContainer>
     </LinearGradient>
   );
