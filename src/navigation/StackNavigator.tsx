9183ac98715db75a4ef7f179e5698c8c2e391658
Marvindlls <marvindillenseger@gmail.com>
2025-02-23 20:50:24 +0100

    Refactor StackNavigator to include HomeScreen and update initial route name

--- src/navigation/StackNavigator.tsx
+++ src/navigation/StackNavigator.tsx
@@ -1,20 +1,22 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import Home from '../Screens/Home';
-import Register from '../Screens/Register';
+import Register from '../(auth)/Register';
+import HomeScreen from '../(tabs)';
 
 const Stack = createStackNavigator();
 
 export default function StackNavigator() {
   return (
     <Stack.Navigator
-      initialRouteName="Home"
+      initialRouteName="Index"
       screenOptions={{
-        headerShown: false,
+        headerShown: true,
       }}
     >
       <Stack.Screen name="Home" component={Home} />
       <Stack.Screen name="Register" component={Register} />
+      <Stack.Screen name="Index" component={HomeScreen} />
     </Stack.Navigator>
   );
 }
