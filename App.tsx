83b9bb565310a1bff56e2d4502f09e42975f2048
Marvindlls <marvindillenseger@gmail.com>
2025-02-23 22:36:47 +0100

    Reorder imports in App.tsx for improved clarity

--- App.tsx
+++ App.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { StyleSheet } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
-import { NavigationContainer } from '@react-navigation/native';
-import StackNavigator from './src/navigation/StackNavigator';
+import StackNavigator from './src/navigation/StackNavigator'; // Import de StackNavigator
 import Colors from './src/Components/Colors';
+import { NavigationContainer } from '@react-navigation/native';
 
 export default function App() {
   return (
