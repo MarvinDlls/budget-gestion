bb62572a140bf6c0484ab280bda68872aa15f4da
Marvindlls <marvindillenseger@gmail.com>
2025-02-23 22:36:55 +0100

    Update Supabase URL and anon key for service configuration

--- services/supabase.tsx
+++ services/supabase.tsx
@@ -1,8 +1,8 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://pwxqffsasywnojdzxxmv.supabase.co'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB3eHFmZnNhc3l3bm9qZHp4eG12Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzc2NDQ0ODAsImV4cCI6MjA1MzIyMDQ4MH0.EIga48OyxDVUxIG7worYaqvYRzJGZVQwQmetVp-EXFk'
+const supabaseUrl = 'https://aklbwkzgaczhdduhvilk.supabase.co'
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFrbGJ3a3pnYWN6aGRkdWh2aWxrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDAzNDIzODUsImV4cCI6MjA1NTkxODM4NX0.Y5NtaET0NA0BzX2_6UAQZt9zOP7Js-h-zuO8xSMab7s'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
