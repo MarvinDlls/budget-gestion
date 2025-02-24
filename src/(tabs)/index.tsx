1eabd48e691ec5ddfe8178756da269ddf5fe07b6
Marvindlls <marvindillenseger@gmail.com>
2025-02-24 22:08:32 +0100

    Remove authentication screens and navigation layout

--- src/(tabs)/index.tsx
+++
@@ -1,9 +0,0 @@
-import { View, Text } from "react-native";
-
-export default function HomeScreen() {
-  return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>Bienvenue sur la Page d'Accueil !</Text>
-    </View>
-  );
-}
