b844e7991ff4e0125ac066d5e2de4680d0dedd5d
Marvindlls <marvindillenseger@gmail.com>
2025-02-26 16:02:44 +0100

    Rename HomeScreen component to IndexScreen for consistency in naming

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,6 +1,6 @@
 import { View, Text } from "react-native";
 
-export default function HomeScreen() {
+export default function IndexScreen() {
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
       <Text>Bienvenue sur la Page d'Accueil !</Text>
