b70e822810bd7b06345ecfe58a49a1cd27009b6d
Marvindlls <marvindillenseger@gmail.com>
2025-03-07 10:33:33 +0100

    Integrate theme context into IndexScreen for dynamic background color

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,20 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
+import { useTheme } from "../../src/context/ThemeContext";
 
 export default function IndexScreen() {
+  const { theme } = useTheme();
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>Bienvenue sur la Page d'Accueil !</Text>
+    <View style={[styles.container, { backgroundColor: theme.backgroundColor }]}>
+      <Text>Bienvenue sur la Page d'Accueil</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#F5F7FA',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+});
